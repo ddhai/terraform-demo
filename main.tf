cba5e609471941ef5699bd177b95ffe930acea28
naviat01 <haidam.ict@gmail.com>
2017-06-30 11:36:38 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config {
-    bucket = "terraform-state-prod"
-    key    = "vpc/terraform.tfstate"
+    bucket = " tf-demo-co-well"
+    key    = "terraform.tfstate"
     region = "us-west-2"
   }
 }
@@ -11,11 +11,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-
 resource "aws_vpc" "minimal" {
-  cidr_block = "${var.vpc_cidr}"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+cidr_block = "${var.vpc_cidr}"
+enable_dns_support   = true
+enable_dns_hostnames = true
   tags {
     Name = "minimal-lab"
   }
