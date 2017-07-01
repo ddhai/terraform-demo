ff7afb13d29b3f15d4a4521e455647b941d0327f
naviat01 <haidam.ict@gmail.com>
2017-07-01 19:28:26 +0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,13 @@
-terraform {
-  backend "s3" {
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config {
     bucket = "tf-demo-co-well"
-    key    = "terraform.tfstate"
+    key = "stage/vpc/terraform.tfstate"
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
-}
-
 resource "aws_vpc" "minimal" {
-cidr_block = "${var.vpc_cidr}"
+cidr_block = "${data.terraform_remote_state.vpc.vpc_cidr"
 enable_dns_support   = true
 enable_dns_hostnames = true
   tags {
