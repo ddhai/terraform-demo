3a1d45b810129b084dd608bb8ba10f5380fee6c0
naviat01 <haidam.ict@gmail.com>
2017-07-01 22:51:45 +0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-demo-co-well"
+    key    = "terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
 resource "aws_vpc" "minimal" {
-cidr_block = "${var.vpc_cidr"}
-enable_dns_support   = true
-enable_dns_hostnames = true
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags {
     Name = "minimal-lab"
   }
